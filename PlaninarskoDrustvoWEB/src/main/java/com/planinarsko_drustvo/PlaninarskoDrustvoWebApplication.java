1ded4a645c4147723d7e0342986ec8193e557512
CupakabraNo1 <vldnovak555@gmail.com>
2020-01-16 23:54:37 +0100

    start version

---
+++ PlaninarskoDrustvoWEB/src/main/java/com/planinarsko_drustvo/PlaninarskoDrustvoWebApplication.java
@@ -0,0 +1,13 @@
+package com.planinarsko_drustvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaninarskoDrustvoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaninarskoDrustvoWebApplication.class, args);
+	}
+
+}
